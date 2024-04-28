a37b1b3e892e548c464430f30ace65a2e71712c8
adityarana835 <adityarana835@gmail.com>
2024-04-29 00:12:42 +0530

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,75 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Aditya",
+		name: "Aditya Rana",
+		email: "adityarana835@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web developer.",
+		description:
+			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Aditya Rana. I live in India, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Flappy Bird Game",
+			description:
+				"Flappy Bird's appeal lies in its simplicity, challenging gameplay, and retro graphics reminiscent of classic arcade games.Flappy Bird in JavaScript involves using HTML5 canvas for rendering graphics and JavaScript for game logic. ",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Library Management System",
+			description:
+				"Library Management System (LMS) in Python is a software application designed to help libraries efficiently manage their resources, including books, journals, magazines, CDs, DVDs, and more.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Furniture Website",
+			description:
+				"furniture website using HTML, CSS, and Bootstrap can result in a visually appealing and responsive platform for showcasing furniture products. Utilize Bootstrap components to enhance the layout and design of your website.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Sudoku Game",
+			description:
+				"Sudoku game in JavaScript involves building the game logic, implementing the user interface, and providing functionality for players to interact with the game. ",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
